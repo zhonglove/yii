7ea352ae48effa9624f7fb6f90bb36124f241af0
vlily <976651023@qq.com>
2020-04-05 13:44:26 +0800

    init

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=qdm722903649.my3w.com;dbname=qdm722903649_db',
+    'username' => 'qdm722903649',
+    'password' => 'S2018731OR3fd35c3',
+    'charset' => 'utf8',
+
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '123456',
+    // 'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ controllers/CountryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+    public function actionTest($message = 'Hello'){
+        $conn=\yii::$app->db;
+        $sql="select * from country";
+        $command=$conn->createCommand($sql);
+        $data=$command->queryOne();
+        return $res=json_encode($data);
+    }
+}
